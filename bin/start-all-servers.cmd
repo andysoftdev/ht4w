bdcc9fc03cb4da8be1db660022cc907dfd456379
andy <andy.softdev@bluewin.ch>
2010-11-03 01:51:38 +0100

    Start servers minimized

--- bin/start-all-servers.cmd
+++ bin/start-all-servers.cmd
@@ -39,12 +39,18 @@ set bin=%~dp0\..\dist\x64\Debug\
 goto start
 
 :default
-set params=-l error
+set params=%1 %2 %3 %4 %5 %6 %7 %8 %9
+if "%1" == "" (
+	set params=-l error
+)
 set bin=%~dp0\..\dist\Win32\Release\
 goto start
 
 :default_debug
-set params=-l error
+set params=%2 %3 %4 %5 %6 %7 %8 %9
+if "%2" == "" (
+	set params=-l error
+)
 set bin=%~dp0\..\dist\Win32\Debug\
 goto start
 
@@ -58,36 +64,36 @@ if not exist %bin%\serverup.exe goto :missing_exe
 
 echo Starting Hypertable.LocalBroker...
 if not exist %bin%\Hypertable.LocalBroker.exe goto :missing_exe
-@start cmd /c "%bin%\Hypertable.LocalBroker.exe %params%"
-@ping 127.0.0.1 -n 1 -w 1000 > nul
+@start "Hypertable.LocalBroker" /min cmd /c "%bin%\Hypertable.LocalBroker.exe %params%"
+@ping 127.0.0.1 -n 2 -w 1000 > nul
 @%bin%\serverup.exe --silent --wait 5000 dfsbroker
 if "%ERRORLEVEL%" NEQ "0" goto failed
 
 echo Starting Hyperspace.Master...
 if not exist %bin%\Hyperspace.Master.exe goto :missing_exe
-@start cmd /c "%bin%\Hyperspace.Master.exe %params%"
+@start "Hyperspace.Master" /min cmd /c "%bin%\Hyperspace.Master.exe %params%"
 @ping 127.0.0.1 -n 3 -w 1000 > nul
 @%bin%\serverup.exe --silent --wait 5000 hyperspace
 if "%ERRORLEVEL%" NEQ "0" goto failed
 
 echo Starting Hypertable.Master...
 if not exist %bin%\Hypertable.Master.exe goto :missing_exe
-@start cmd /c "%bin%\Hypertable.Master.exe %params%"
-@ping 127.0.0.1 -n 2 -w 1000 > nul
+@start "Hypertable.Master" /min cmd /c "%bin%\Hypertable.Master.exe %params%"
+@ping 127.0.0.1 -n 3 -w 1000 > nul
 @%bin%\serverup.exe --silent --wait 5000 master
 if "%ERRORLEVEL%" NEQ "0" goto failed
 
 echo Starting Hypertable.RangeServer...
 if not exist %bin%\Hypertable.RangeServer.exe goto :missing_exe
-@start cmd /c "%bin%\Hypertable.RangeServer.exe %params%"
-@ping 127.0.0.1 -n 2 -w 1000 > nul
+@start "Hypertable.RangeServer" /min cmd /c "%bin%\Hypertable.RangeServer.exe %params%"
+@ping 127.0.0.1 -n 3 -w 1000 > nul
 @%bin%\serverup.exe --silent --wait 5000 rangeserver
 if "%ERRORLEVEL%" NEQ "0" goto failed
 
 echo Starting Hypertable.ThriftBroker...
 if not exist %bin%\Hypertable.ThriftBroker.exe goto :missing_exe
-@start cmd /c "%bin%\Hypertable.ThriftBroker.exe %params%"
-@ping 127.0.0.1 -n 1 -w 1000 > nul
+@start "Hypertable.ThriftBroker" /min cmd /c "%bin%\Hypertable.ThriftBroker.exe %params%"
+@ping 127.0.0.1 -n 2 -w 1000 > nul
 @%bin%\serverup.exe --silent --wait 5000 thriftbroker
 if "%ERRORLEVEL%" NEQ "0" goto failed
 
