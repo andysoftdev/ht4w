898c220d3ef9a78bdb828fa84a905a1dc8112f7c
andy <andy.softdev@bluewin.ch>
2011-01-13 21:24:21 +0100

    Make use of #pragma comment( lib, ... ) for widely used libraries

--- src/cc/stdafx.cc
+++ src/cc/stdafx.cc
@@ -1,2 +1,15 @@
 // create pch
 #include "Common/Compat.h"
+
+#ifndef _LIB
+
+#pragma comment( lib, "sigar.lib" )
+#pragma comment( lib, "log4cpp.lib" )
+#pragma comment( lib, "zlib.lib" )
+#pragma comment( lib, "advapi32.lib" )
+#pragma comment( lib, "shell32.lib" )
+#pragma comment( lib, "ws2_32.lib" )
+#pragma comment( lib, "netapi32.lib" )
+#pragma comment( lib, "version.lib" )
+
+#endif
