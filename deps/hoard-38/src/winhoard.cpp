00685761d8048477baa9ebe0c4ff642741d97b58
andy <andy.softdev@bluewin.ch>
2011-03-27 16:42:28 +0200

    _exit patch has been fixed

--- deps/hoard-38/src/winhoard.cpp
+++ deps/hoard-38/src/winhoard.cpp
@@ -1,34 +1,34 @@
 /* -*- C++ -*- */
 
 /*
-  The Hoard Multiprocessor Memory Allocator
-  www.hoard.org
+The Hoard Multiprocessor Memory Allocator
+www.hoard.org
 
-  Author: Emery Berger, http://www.cs.umass.edu/~emery
- 
-  Copyright (c) 1998-2008 Emery Berger, The University of Texas at Austin
+Author: Emery Berger, http://www.cs.umass.edu/~emery
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+Copyright (c) 1998-2008 Emery Berger, The University of Texas at Austin
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
 /*
 
-  Compile with compile-winhoard.cmd.
+Compile with compile-winhoard.cmd.
 
- */
+*/
 
 
 #include <windows.h>
@@ -45,9 +45,6 @@
 #pragma warning(disable: 4098)  // Library conflict.
 #pragma warning(disable: 4355)  // 'this' used in base member initializer list.
 #pragma warning(disable: 4074)	// initializers put in compiler reserved area.
-
-static void (*hoard_memcpy_ptr)(void *dest, const void *src, size_t count) = 0;
-static void (*hoard_memset_ptr)(void *dest, int c, size_t count) = 0;
 
 #ifndef _DEBUG
 static const char *RlsCRTLibraryName[] = { /*"MSVCR71.DLL", "MSVCR80.DLL", "MSVCR90.DLL",*/ "MSVCR100.DLL", 0 };
@@ -66,11 +63,11 @@ typedef struct
   FARPROC replacement;		// pointer to replacement function
   FARPROC original;		// pointer to original function
   unsigned char codebytes[BytesToStore];	// original code storage
+  HMODULE module;
 } PATCH;
 
 
 static bool PatchMeIn (void);
-static bool PatchMeOut (void);
 static void PatchIt (PATCH *patch);
 static void UnPatchIt (PATCH *patch);
 
@@ -97,59 +94,59 @@ extern "C" {
 /* ------------------------------------------------------------------------ */
 
 static PATCH rls_patches[] = 
-  {
+{
 
-    // RELEASE CRT library routines supported by this memory manager.
+  // RELEASE CRT library routines supported by this memory manager.
 
-    {"exit",            (FARPROC) hoard_exit,      0},
-    {"_exit",           (FARPROC) hoard__exit,     0},
-    {"_onexit",         (FARPROC) hoard_onexit,    0},
-    {"_expand",         (FARPROC) hoard_expand,	   0},
+  {"exit",            (FARPROC) hoard_exit,      0, 0},
+  {"_exit",           (FARPROC) hoard__exit,     0, 0},
+  {"_onexit",         (FARPROC) hoard_onexit,    0, 0},
+  {"_expand",         (FARPROC) hoard_expand,	   0, 0},
 
-    // operator new, new[], delete, delete[].
+  // operator new, new[], delete, delete[].
 
 #ifdef _WIN64
 
-    {"??2@YAPEAX_K@Z",  (FARPROC) hoard_malloc,    0},
-    {"??_U@YAPEAX_K@Z", (FARPROC) hoard_malloc,    0},
-    {"??3@YAXPEAX@Z",   (FARPROC) hoard_free,      0},
-    {"??_V@YAXPEAX@Z",  (FARPROC) hoard_free,      0},
+  {"??2@YAPEAX_K@Z",  (FARPROC) hoard_malloc,    0, 0},
+  {"??_U@YAPEAX_K@Z", (FARPROC) hoard_malloc,    0, 0},
+  {"??3@YAXPEAX@Z",   (FARPROC) hoard_free,      0, 0},
+  {"??_V@YAXPEAX@Z",  (FARPROC) hoard_free,      0, 0},
 
 #else
 
-    {"??2@YAPAXI@Z",    (FARPROC) hoard_malloc,    0},
-    {"??_U@YAPAXI@Z",   (FARPROC) hoard_malloc,    0},
-    {"??3@YAXPAX@Z",    (FARPROC) hoard_free,      0},
-    {"??_V@YAXPAX@Z",   (FARPROC) hoard_free,      0},
+  {"??2@YAPAXI@Z",    (FARPROC) hoard_malloc,    0, 0},
+  {"??_U@YAPAXI@Z",   (FARPROC) hoard_malloc,    0, 0},
+  {"??3@YAXPAX@Z",    (FARPROC) hoard_free,      0, 0},
+  {"??_V@YAXPAX@Z",   (FARPROC) hoard_free,      0, 0},
 
 #endif
 
-    // comment from tcmalloc
+  // comment from tcmalloc
 
-    // Ideally we should patch the nothrow versions of new/delete, but
-    // at least in msvcrt, nothrow-new machine-code is of a type we
-    // can't patch.  Since these are relatively rare, I'm hoping it's ok
-    // not to patch them.
+  // Ideally we should patch the nothrow versions of new/delete, but
+  // at least in msvcrt, nothrow-new machine-code is of a type we
+  // can't patch.  Since these are relatively rare, I'm hoping it's ok
+  // not to patch them.
 
-    // the nothrow variants new, new[].
+  // the nothrow variants new, new[].
 
-    //{"??2@YAPAXIABUnothrow_t@std@@@Z",  (FARPROC) hoard_malloc, 0},
-    //{"??_U@YAPAXIABUnothrow_t@std@@@Z", (FARPROC) hoard_malloc, 0},
+  //{"??2@YAPAXIABUnothrow_t@std@@@Z",  (FARPROC) hoard_malloc, 0, 0},
+  //{"??_U@YAPAXIABUnothrow_t@std@@@Z", (FARPROC) hoard_malloc, 0, 0},
 
-    // the nothrow variants delete, delete[].
+  // the nothrow variants delete, delete[].
 
-    //{"??3@YAXPAXABUnothrow_t@std@@@Z",  (FARPROC) hoard_malloc, 0},
-    //{"??_V@YAXPAXABUnothrow_t@std@@@Z", (FARPROC) hoard_malloc, 0},
+  //{"??3@YAXPAXABUnothrow_t@std@@@Z",  (FARPROC) hoard_malloc, 0, 0},
+  //{"??_V@YAXPAXABUnothrow_t@std@@@Z", (FARPROC) hoard_malloc, 0, 0},
 
-    {"_msize",	(FARPROC) hoard_malloc_usable_size,		0},
-    {"calloc",	(FARPROC) hoard_calloc,		0},
-    {"malloc",	(FARPROC) hoard_malloc,		0},
-    {"realloc",	(FARPROC) hoard_realloc,		0},
-    {"free",	(FARPROC) hoard_free,              0},
-    {"_recalloc", (FARPROC) hoard_recalloc, 0},
-    {"_putenv",	(FARPROC) hoard__putenv,		0},
-    {"getenv",	(FARPROC) hoard_getenv,		0},
-  };
+  {"_msize",	(FARPROC) hoard_malloc_usable_size, 0, 0},
+  {"calloc",	(FARPROC) hoard_calloc,     0, 0},
+  {"malloc",	(FARPROC) hoard_malloc,     0, 0},
+  {"realloc",	(FARPROC) hoard_realloc,    0, 0},
+  {"free",	(FARPROC) hoard_free,         0, 0},
+  {"_recalloc", (FARPROC) hoard_recalloc, 0, 0},
+  {"_putenv",	(FARPROC) hoard__putenv,    0, 0},
+  {"getenv",	(FARPROC) hoard_getenv,     0, 0},
+};
 
 
 static void PatchIt (PATCH *patch)
@@ -158,23 +155,23 @@ static void PatchIt (PATCH *patch)
 
   MEMORY_BASIC_INFORMATION mbi_thunk;
   VirtualQuery((void*)patch->original, &mbi_thunk, 
-	       sizeof(MEMORY_BASIC_INFORMATION));
+    sizeof(MEMORY_BASIC_INFORMATION));
   VirtualProtect(mbi_thunk.BaseAddress, mbi_thunk.RegionSize, 
-		 PAGE_EXECUTE_READWRITE, &mbi_thunk.Protect);
+    PAGE_EXECUTE_READWRITE, &mbi_thunk.Protect);
 
   // Patch CRT library original routine:
   // 	save original code bytes for exit restoration
   //		write jmp <patch_routine> (at least 5 bytes long) to original.
 
-  (*hoard_memcpy_ptr)(patch->codebytes, patch->original, sizeof(patch->codebytes));
+  CopyMemory(patch->codebytes, patch->original, sizeof(patch->codebytes));
   unsigned char *patchloc = (unsigned char*)patch->original;
   *patchloc++ = IAX86_NEARJMP_OPCODE;
   *(unsigned*)patchloc = MakeIAX86Offset(patch->replacement, patch->original);
-	
+
   // Reset CRT library code to original page protection.
 
   VirtualProtect(mbi_thunk.BaseAddress, mbi_thunk.RegionSize, 
-		 mbi_thunk.Protect, &mbi_thunk.Protect);
+    mbi_thunk.Protect, &mbi_thunk.Protect);
 }
 
 static void UnPatchIt (PATCH *patch)
@@ -183,88 +180,45 @@ static void UnPatchIt (PATCH *patch)
 
   MEMORY_BASIC_INFORMATION mbi_thunk;
   VirtualQuery((void*)patch->original, &mbi_thunk, 
-	       sizeof(MEMORY_BASIC_INFORMATION));
+    sizeof(MEMORY_BASIC_INFORMATION));
   VirtualProtect(mbi_thunk.BaseAddress, mbi_thunk.RegionSize, 
-		 PAGE_EXECUTE_READWRITE, &mbi_thunk.Protect);
+    PAGE_EXECUTE_READWRITE, &mbi_thunk.Protect);
 
   // Patch CRT library original routine:
   // 	save original code bytes for exit restoration
   //		write jmp <patch_routine> (at least 5 bytes long) to original.
 
-  (*hoard_memcpy_ptr)(patch->original, patch->codebytes, sizeof(patch->codebytes));
+  CopyMemory(patch->original, patch->codebytes, sizeof(patch->codebytes));
 
   // Reset CRT library code to original page protection.
 
   VirtualProtect(mbi_thunk.BaseAddress, mbi_thunk.RegionSize, 
-		 mbi_thunk.Protect, &mbi_thunk.Protect);
+    mbi_thunk.Protect, &mbi_thunk.Protect);
 }
 
 
 static bool PatchMeIn (void)
 {
   bool patchedIn = false;
-  
+
   // acquire the module handles for the CRT libraries (release and debug)
   for (int i = 0; RlsCRTLibraryName[i]; i++) {
-
     HMODULE RlsCRTLibrary = GetModuleHandle(RlsCRTLibraryName[i]);
-
-    HMODULE DefCRTLibrary = 
-      RlsCRTLibrary;
-
-    // assign function pointers for required CRT support functions
-    if (DefCRTLibrary) {
-      hoard_memcpy_ptr = (void(*)(void*,const void*,size_t))
-	GetProcAddress(DefCRTLibrary, "memcpy");
-      hoard_memset_ptr = (void(*)(void*,int,size_t))
-	GetProcAddress(DefCRTLibrary, "memset");
-    }
 
     // patch all relevant Release CRT Library entry points
     if (RlsCRTLibrary) {
       for (int i = 0; i < sizeof(rls_patches) / sizeof(*rls_patches); i++) {
-	if (rls_patches[i].original = GetProcAddress(RlsCRTLibrary, rls_patches[i].import)) {
-	  PatchIt(&rls_patches[i]);
- 	  patchedIn = true;
-	}
+        if (rls_patches[i].original = GetProcAddress(RlsCRTLibrary, rls_patches[i].import)) {
+          rls_patches[i].module = RlsCRTLibrary;
+          PatchIt(&rls_patches[i]);
+          patchedIn = true;
+        }
       }
     }
   }
   return patchedIn;
 }
 
-static bool PatchMeOut (void)
-{
-  bool patchedOut = false;
-  
-  // acquire the module handles for the CRT libraries (release and debug)
-  for (int i = 0; RlsCRTLibraryName[i]; i++) {
-
-    HMODULE RlsCRTLibrary = GetModuleHandle(RlsCRTLibraryName[i]);
-
-    HMODULE DefCRTLibrary = 
-      RlsCRTLibrary;
-
-    // assign function pointers for required CRT support functions
-    if (DefCRTLibrary) {
-      hoard_memcpy_ptr = (void(*)(void*,const void*,size_t))
-	GetProcAddress(DefCRTLibrary, "memcpy");
-      hoard_memset_ptr = (void(*)(void*,int,size_t))
-	GetProcAddress(DefCRTLibrary, "memset");
-    }
-
-    // patch all relevant Release CRT Library entry points
-    if (RlsCRTLibrary) {
-      for (int i = 0; i < sizeof(rls_patches) / sizeof(*rls_patches); i++) {
-	if (rls_patches[i].original = GetProcAddress(RlsCRTLibrary, rls_patches[i].import)) {
-	  UnPatchIt(&rls_patches[i]);
- 	  patchedOut = true;
-	}
-      }
-    }
-  }
-  return patchedOut;
-}
 
 // Intercept the exit functions.
 
@@ -292,19 +246,26 @@ extern "C" {
   }
 
   void hoard_exit (int code) {
-    HL::Guard<TheLockType> g (exitLock);
-    while (exitCount > 0) {
-      exitCount--;
-      (_onexit_buf[exitCount])();
+    exit_t fp;
+    {
+      HL::Guard<TheLockType> g (exitLock);
+      while (exitCount > 0) {
+        exitCount--;
+        (_onexit_buf[exitCount])();
+      }
+      UnPatchIt(&rls_patches[0]);
+      fp = (exit_t)rls_patches[0].original;
     }
-    PatchMeOut();
-    exit_t fp = (exit_t)rls_patches[0].original;
     (*fp)(code);
   }
 
   void hoard__exit (int code) {
-    PatchMeOut();
-    exit_t fp = (exit_t)rls_patches[0].original;
+    exit_t fp;
+    {
+      HL::Guard<TheLockType> g (exitLock);
+      UnPatchIt(&rls_patches[1]);
+      fp = (exit_t)rls_patches[1].original;
+    }
     (*fp)(code);
   }
 
