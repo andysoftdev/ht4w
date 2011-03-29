335ccf063bf07e9b17b054e87bfdd2df96dea5fe
andy <andy.softdev@bluewin.ch>
2011-03-29 22:49:08 +0200

    kill servers batch file has added

---
+++ bin/kill-all-servers.cmd
@@ -0,0 +1,52 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set platform=Win32
+set configuration=Release
+set params=
+
+for %%p in (%*) do (
+	if /i "%%~p" == "x86" (
+		set platform=Win32
+	) else (
+		if /i "%%~p" == "win32" (
+			set platform=Win32
+		) else (
+			if /i "%%~p" == "x64" (
+				set platform=x64
+			) else (
+				if /i "%%~p" == "win64" (
+					set platform=x64					
+				) else (
+					if /i "%%~p" == "release" (
+						set configuration=Release
+					) else (
+						if /i "%%~p" == "debug" (
+							set configuration=Debug
+						) else (							
+							set params=!params! %%p
+						)
+					)
+				)
+			)
+		)
+	)
+)
+
+set bin=%~dp0..\dist\%platform%\%configuration%\
+
+if not exist %bin%\conf md %bin%\conf > nul
+if not exist %bin%\conf\hypertable.cfg xcopy %~dp0..\conf\hypertable.cfg %bin%\conf\ /Q /R /Y /D > nul
+xcopy %~dp0..\conf\METADATA.xml %bin%\conf\ /Q /R /Y /D > nul
+xcopy %~dp0..\conf\RS_METRICS.xml %bin%\conf\ /Q /R /Y /D > nul
+
+if not exist %bin%\hypertable.service.exe goto :missing_exe
+@%bin%\hypertable.service.exe --stop-all-services --kill-servers %params%
+
+goto done
+
+:missing_exe
+echo hypertable.service.exe does not exists
+goto done:
+
+:done
