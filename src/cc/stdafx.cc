3dc3b802630022f506e1f088ba4de40ad3549690
andy <andy.softdev@bluewin.ch>
2013-04-24 20:05:29 +0200

    log4cpp dependency removed

--- src/cc/stdafx.cc
+++ src/cc/stdafx.cc
@@ -24,7 +24,6 @@
 #ifndef _LIB
 
 #pragma comment( lib, "sigar.lib" )
-#pragma comment( lib, "log4cpp.lib" )
 #pragma comment( lib, "zlib.lib" )
 #pragma comment( lib, "advapi32.lib" )
 #pragma comment( lib, "shell32.lib" )
