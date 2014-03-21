ef521af282f81ef61d2764022ea8c2c245d8ce37
andy <andy.softdev@bluewin.ch>
2014-03-21 16:56:26 +0100

    Version updated to 0.9.7.17

--- src/cc/Common/Version.h
+++ src/cc/Common/Version.h
@@ -28,9 +28,9 @@
 #define HT_VERSION_MAJOR        0
 #define HT_VERSION_MINOR        9
 #define HT_VERSION_MICRO        7
-#define HT_VERSION_PATCH        16
+#define HT_VERSION_PATCH        17
 #define HT_VERSION_MISC_SUFFIX  ""
-#define HT_VERSION_STRING       "0.9.7.16"
+#define HT_VERSION_STRING       "0.9.7.17"
 
 namespace Hypertable {
   extern const int version_major;
