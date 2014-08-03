eaeb292396d4a63b1336c13315b2709c0c07635f
andy <andy.softdev@bluewin.ch>
2014-08-03 11:05:34 +0200

    Use stable_sort to sort keys in client mutator buffer

--- src/cc/Hypertable/Lib/TableMutatorAsyncScatterBuffer.cc
+++ src/cc/Hypertable/Lib/TableMutatorAsyncScatterBuffer.cc
@@ -275,7 +275,7 @@ void TableMutatorAsyncScatterBuffer::send(uint32_t flags) {
         send_rec.offset = send_buffer->key_offsets[i];
         send_vec.push_back(send_rec);
       }
-      sort(send_vec.begin(), send_vec.end());
+      std::stable_sort(send_vec.begin(), send_vec.end());
 
       ptr = send_buffer->pending_updates.base;
 
